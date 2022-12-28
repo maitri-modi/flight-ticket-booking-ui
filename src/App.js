e9d10ec411bc2650c879ca9f6ea656e258f281ef
maitri_modi <maitrimodi001@gmail.com>
2022-12-28 17:02:46 +0530

    [Maitri] Implementing home page, header and footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { ExploreFlights } from './layouts/homePage/ExploreFights';
+import { Footer } from './layouts/navbarAndFooter/Footer';
+import { Navbar } from './layouts/navbarAndFooter/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <ExploreFlights/>
+      <Footer/>
     </div>
   );
 }
